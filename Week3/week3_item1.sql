bb7b951a27c9b7705260238ed84574ce60ef499d
Ilhan Hashi <I.saidhashi@gmail.com>
2026-04-14 22:09:55 -0500

    Add files via upload
    week 3 items 1 file for number E

---
+++ Week3/week3_item1.sql
@@ -0,0 +1,56 @@
+USE Restaurant;
+GO
+
+/* 
+	Item A 
+*/
+
+
+/* 
+	Item B
+*/
+
+
+/* 
+	Item C 
+*/
+
+
+/* 
+	Item D 
+*/
+
+
+/* 
+	Item E: Drops the function if it already ecists before recrating it.
+*/
+DROP FUNCTION IF EXISTS dbo.fn_FavoriteTable;
+GO
+
+-- Returns a count of patrons who revieced their favorite table vs those 
+-- who did not.
+CREATE FUNCTION dbo.fn_FavoriteTable()
+RETURNS TABLE 
+AS 
+RETURN
+(
+	SELECT 
+		CASE WHEN TableChosen = TablePreference
+		THEN 'YES'
+		ELSE 'NO'
+	END AS GotFavoriteTable,
+	COUNT(*) AS PatronCount
+
+	FROM Reservations
+	GROUP BY 
+		CASE WHEN TableChosen = TablePreference
+		THEN 'YES'
+		ELSE 'NO'
+		END
+);
+GO
+
+-- Test
+SELECT * 
+FROM dbo.fn_FavoriteTable()
+
