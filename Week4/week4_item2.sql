a185a18f9beb019700f91d6e7775d36e7f68544c
Ilhan Hashi <I.saidhashi@gmail.com>
2026-04-18 18:40:33 -0500

    Update week4_item2.sql
    Item B done.

--- Week4/week4_item2.sql
+++ Week4/week4_item2.sql
@@ -6,7 +6,17 @@ GO
 
 
 -- B.
+-- Index on DishName on Dishes.
+CREATE NONCLUSTERED INDEX IX_Dishes_DishName
+ON Dishes (DishName);
 
+-- Index on LocationID on Dishes.
+CREATE NONCLUSTERED INDEX IX_Dishes_LocationID
+ON Dishes (LocationID);
+
+-- Index on Price on Dishes.
+CREATE NONCLUSTERED INDEX IX_Dishes_Price
+ON Dishes (Price);
 
 -- C.
 
