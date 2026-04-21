e33729a3eb31356670b15a886e16adcd858f38cf
Pedro Henrique Gomes <149111011+Phpadawan@users.noreply.github.com>
2026-04-21 16:59:15 -0500

    Add multiple stored procedures for recipe and kitchen details

--- Week4/week4_item1.sql
+++ Week4/week4_item1.sql
@@ -23,9 +23,69 @@ EXECUTE spN_ChefDetails;
 
 
 -- C.
-
+CREATE PROC spN_RecipeDetails
+AS
+BEGIN
+    SELECT 
+        r.RecipeID,
+        r.DishID,
+        d.DishName,
+        d.Description,
+        r.IngredientID,
+        i.IngredientName,
+        r.IngredientQuantity,
+        r.Unit,
+        FORMAT(d.Price, 'C2', 'EN-GB') AS Price
+    FROM Recipes r
+        INNER JOIN Ingredients i
+            ON r.IngredientID = i.IngredientID
+        INNER JOIN Dishes d
+            ON r.DishID = d.DishID
+    ORDER BY d.DishName, i.IngredientName
+END;
+GO
+EXECUTE spN_RecipeDetails;
 
 -- D.
+CREATE PROC spN_KitchenDetails
+   -- pass a ChefID for a specific chef
+   @ChefID INT = NULL  
+AS
+BEGIN
+    SELECT
+        kd.KitchenID,
+        kd.NumStoves,
+        kd.MinNumCooks,
+        l.LocationID,
+        l.Address,
+        l.PostTown,
+        l.County,
+        l.EIRCode,
+        kd.HeadChef,
+        kd.SousChef,
+        c.ChefID,
+        cl.EmployeeID,
+        cl.ChefPosition,
+        FORMAT(d.Price, 'C2', 'EN-GB') AS Price
+    FROM KitchenDetails kd
+    -- Join to get location details, chef details, and dish prices for each kitchen
+        INNER JOIN Locations l
+            ON kd.LocationID = l.LocationID
+        INNER JOIN ChefLocations cl
+            ON cl.LocationID = l.LocationID
+        INNER JOIN Chefs c
+            ON c.EmployeeID = cl.EmployeeID
+        INNER JOIN Dishes d
+            ON d.LocationID = l.LocationID
+    WHERE (@ChefID IS NULL OR c.ChefID = @ChefID)
+    ORDER BY kd.KitchenID, c.ChefID
+END;
+GO
 
+-- All records
+EXECUTE spN_KitchenDetails;
+
+-- Specific chef
+EXECUTE spN_KitchenDetails @ChefID = 1;
 
 -- E.
