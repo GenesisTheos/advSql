a434db9d1e7a4258f9c80c861ee1ca7c10f89794
GenesisTheos <chandlerfrea@icloud.com>
2026-04-21 10:20:02 -0500

    Update week4_item2.sql

--- Week4/week4_item2.sql
+++ Week4/week4_item2.sql
@@ -3,6 +3,18 @@ GO
 
 /** Item 2 Indexes**/
 -- A.
+--2a Indexes for Charities Table
+--Index on CharityName
+CREATE NONCLUSTERED INDEX IX_Charities_CharityName
+ON Charities(CharityName);
+
+--Index on CharityAddress
+CREATE NONCLUSTERED INDEX IX_Charities_CharityAddress
+ON Charities(CharityAddress);
+
+--Index on PhoneNum
+CREATE NONCLUSTERED INDEX IX_Charities_PhoneNum
+ON Charities(PhoneNum);
 
 
 -- B.
