57fde9f8435cdf12de9d28db6de9628d2bc290f4
GenesisTheos <chandlerfrea@icloud.com>
2026-04-21 10:19:10 -0500

    Update week4_item1.sql

--- Week4/week4_item1.sql
+++ Week4/week4_item1.sql
@@ -3,7 +3,21 @@ GO
 
 /** Item 1 5 Stored Procs **/
 -- A.
+--1a creates a stored proc to get the chefs who have a preferred vendor and the cost of those ingredients from the vendor
+CREATE PROC spN_ChefDetails
+AS
+BEGIN
+	SELECT ChefID, IngredientName, i.SupplierID, FORMAT(i.IngredientPrice, 'C2', 'EN-GB') AS Price
+	FROM Chefs c
+	INNER JOIN Supplier s
+		ON c.PreferredSupplier = s.SupplierID
+	INNER JOIN Ingredients i
+		ON i.SupplierID = s.SupplierID
+	WHERE C.PreferredSupplier IS NOT NULL
+	ORDER BY ChefID
+END;
 
+EXECUTE spN_ChefDetails;
 
 -- B.
 
