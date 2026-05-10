b7db573b65f83172ec68f4e743eb3570071a2153
Ilhan Hashi <I.saidhashi@gmail.com>
2026-05-10 17:32:32 -0500

    Add files via upload
    Items 2 file

---
+++ Week7_Item2.sql
@@ -0,0 +1,11 @@
+USE Restaurant;
+GO 
+
+-- A 
+-- B 
+-- C
+-- D 
+
+-- E: Assigns control permession to RestaurantDoAnything on sPN_CustomerDetails.
+GRANT CONTROL ON OBJECT::dbo.sPN_CustomerDetails To RestaurantDoAnything;
+GO
