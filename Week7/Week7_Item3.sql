ba7fa5c09aec908c464e83c486c796308b271b98
GenesisTheos <chandlerfrea@icloud.com>
2026-05-10 18:19:58 -0500

    Update Week7_Item3.sql

--- Week7/Week7_Item3.sql
+++ Week7/Week7_Item3.sql
@@ -1,6 +1,11 @@
 USE Restaurant;	
 GO
 
+--A: creates a login and user then assigns it the permission/role of db_datareader
+CREATE LOGIN dishes_table_user WITH PASSWORD = 'DUser';
+CREATE USER dishes_table_user FOR LOGIN dishes_table_user;
+ALTER ROLE db_datareader ADD MEMBER dishes_table_user;
+
 -- E: Creates Reservations_table_user with db_datareader and db_datawriter permissions. 
 CREATE USER Reservations_table_user WITHOUT LOGIN;
 GO
