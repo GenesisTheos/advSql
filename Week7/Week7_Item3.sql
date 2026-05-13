74d0b1b141fdb38cf4de4da3f830b0ea0619b2a9
Pedro Henrique Gomes <149111011+Phpadawan@users.noreply.github.com>
2026-05-12 19:46:04 -0500

    Refactor SQL script for user and role creation

--- Week7/Week7_Item3.sql
+++ Week7/Week7_Item3.sql
@@ -6,6 +6,16 @@ CREATE LOGIN dishes_table_user WITH PASSWORD = 'DUser';
 CREATE USER dishes_table_user FOR LOGIN dishes_table_user;
 ALTER ROLE db_datareader ADD MEMBER dishes_table_user;
 
+
+-- D: Creates chef_table_user  with permissions
+-- Create the role
+CREATE ROLE chefs_table_user;
+GO
+
+-- Add the role to the db_accessadmin database role
+EXEC sp_addrolemember 'db_accessadmin', 'chefs_table_user';
+GO
+
 -- E: Creates Reservations_table_user with db_datareader and db_datawriter permissions. 
 CREATE USER Reservations_table_user WITHOUT LOGIN;
 GO
