966fe9431e3de49966c388b2661b7b7c4e0b7714
Pedro Henrique Gomes <149111011+Phpadawan@users.noreply.github.com>
2026-05-12 20:26:02 -0500

    Refactor SQL permissions and add ownership grant

--- Week7/Week7_Item2.sql
+++ Week7/Week7_Item2.sql
@@ -6,9 +6,13 @@ GRANT EXECUTE
 ON OBJECT::dbo.spN_ChefDetails
 TO dbo
 
--- B 
+-- B
+  
 -- C
--- D 
+  
+-- D Grant TAKE OWNERSHIP on spN_KitchenDetails to RestaurantPower
+  GRANT TAKE OWNERSHIP ON OBJECT::dbo.spN_KitchenDetails TO RestaurantPower;
+GO
 
 -- E: Assigns control permession to RestaurantDoAnything on sPN_CustomerDetails.
 GRANT CONTROL ON OBJECT::dbo.sPN_CustomerDetails To RestaurantDoAnything;
