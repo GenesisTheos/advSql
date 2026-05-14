abcf3e27c9e03a4d4a4c99e946d01271cf7cdd15
Nicole Gonzalez <25304768@nwtc.edu>
2026-05-14 08:04:12 -0500

    2b-2c

--- Week7/Week7_Item2.sql
+++ Week7/Week7_Item2.sql
@@ -6,9 +6,22 @@ GRANT EXECUTE
 ON OBJECT::dbo.spN_ChefDetails
 TO dbo
 
--- B
+-- B Grant VIEW DEFINITION permission to the RestaurantUser for spN_ChefDetails stored proc
+USE Restaurant;
+GO
+GRANT VIEW DEFINITION
+ON OBJECT::dbo.spN_ChefDetails
+TO RestaurantUser;
+GO
   
--- C
+-- C: Assign 'Alter' Permissions to the user 'RestaurantAddDeleteDb' for spN_RecipeDetai stored proc
+USE Restaurant;
+GO
+
+GRANT ALTER
+ON OBJECT::dbo.spN_RecipeDetails
+TO RestaurantAddDeleteDb;
+GO
   
 -- D Grant TAKE OWNERSHIP on spN_KitchenDetails to RestaurantPower
   GRANT TAKE OWNERSHIP ON OBJECT::dbo.spN_KitchenDetails TO RestaurantPower;
